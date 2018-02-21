0e3db936752e02d40f8acc0eb25a5e3e769a6d78
P4ki <paki.aleksandar.93@gmail.com>
2018-02-21 16:11:51 +0100

    breadcrumbs v1 - btn

--- src/components/Breadcrumbs.js
+++ src/components/Breadcrumbs.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, TextInput, Text, View, TouchableOpacity, ImageBackground } from 'react-native';
+import { StyleSheet, TextInput, Text, View, TouchableOpacity, ImageBackground, TouchableHighlight } from 'react-native';
 import _ from 'lodash';
 import he from 'he';
 
@@ -47,16 +47,13 @@ export default class BreadcrumbsComponent extends Component {
 
       <View style={styles.breadcrumbsContainer}>
 
-
-
         <View style={{ flexDirection: 'row', height: '100%', marginLeft: 'auto', marginRight: 'auto', alignItems: 'center' }}>
-          {this.state["1"] && <ImageBackground source={require('../components/ico/breadcrumbs-btn-bg.png')} style={{ height: 80, width: 250,  alignItems: 'center', justifyContent: 'center', }}><Text>{this.state["1"]}</Text></ImageBackground>}
-          {this.state["2"] && <ImageBackground source={require('../components/ico/breadcrumbs-btn-bg.png')} style={{ height: 80, width: 250,  alignItems: 'center', justifyContent: 'center',  }}><Text>{this.state["2"]}</Text></ImageBackground>}
-          {this.state["3"] && <ImageBackground source={require('../components/ico/breadcrumbs-btn-bg.png')} style={{ height: 80, width: 250,  alignItems: 'center', justifyContent: 'center',  }}><Text>{this.state["3"]}</Text></ImageBackground>}
-          {this.state["4"] && <ImageBackground  source={require('../components/ico/breadcrumbs-btn-bg.png')}style={{ height: 80, width: 250,  alignItems: 'center', justifyContent: 'center', }}><Text>{this.state["4"]}</Text></ImageBackground>}
+          {this.state["1"] && <TouchableOpacity><ImageBackground source={require('../components/ico/breadcrumbs-btn-bg.png')} style={{ height: 80, width: 250,  alignItems: 'center', justifyContent: 'center', }}><Text>{this.state["1"]}</Text></ImageBackground></TouchableOpacity>}
+          {this.state["2"] && <TouchableOpacity><ImageBackground source={require('../components/ico/breadcrumbs-btn-bg.png')} style={{ height: 80, width: 250,  alignItems: 'center', justifyContent: 'center',  }}><Text>{this.state["2"]}</Text></ImageBackground></TouchableOpacity>}
+          {this.state["3"] && <TouchableOpacity><ImageBackground source={require('../components/ico/breadcrumbs-btn-bg.png')} style={{ height: 80, width: 250,  alignItems: 'center', justifyContent: 'center',  }}><Text>{this.state["3"]}</Text></ImageBackground></TouchableOpacity>}
+          {this.state["4"] && <TouchableOpacity><ImageBackground  source={require('../components/ico/breadcrumbs-btn-bg.png')}style={{ height: 80, width: 250,  alignItems: 'center', justifyContent: 'center', }}><Text>{this.state["4"]}</Text></ImageBackground></TouchableOpacity>}
         </View>
       </View>
-
 
     );
   }
