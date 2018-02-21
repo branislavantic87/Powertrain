ee1e78782fd7d0c28a288c1702bff8a985e30dd6
P4ki <paki.aleksandar.93@gmail.com>
2018-02-21 16:08:17 +0100

    breadcrumbs v1

--- src/components/Breadcrumbs.js
+++ src/components/Breadcrumbs.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, TextInput, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, TextInput, Text, View, TouchableOpacity, ImageBackground } from 'react-native';
 import _ from 'lodash';
 import he from 'he';
 
@@ -39,21 +39,21 @@ export default class BreadcrumbsComponent extends Component {
     this.setState({ menustr: this.getBreadcrumb(this.props.from) });
 
   }
-rr
+  rr
 
   render() {
     return (
 
 
       <View style={styles.breadcrumbsContainer}>
-        <Text>{this.state.menustr}</Text>
 
 
-        <View>
-          <Text>{this.state["1"]}</Text>
-          <Text>{this.state["2"]}</Text>
-          <Text>{this.state["3"]}</Text>
-          <Text>{this.state["4"]}</Text>
+
+        <View style={{ flexDirection: 'row', height: '100%', marginLeft: 'auto', marginRight: 'auto', alignItems: 'center' }}>
+          {this.state["1"] && <ImageBackground source={require('../components/ico/breadcrumbs-btn-bg.png')} style={{ height: 80, width: 250,  alignItems: 'center', justifyContent: 'center', }}><Text>{this.state["1"]}</Text></ImageBackground>}
+          {this.state["2"] && <ImageBackground source={require('../components/ico/breadcrumbs-btn-bg.png')} style={{ height: 80, width: 250,  alignItems: 'center', justifyContent: 'center',  }}><Text>{this.state["2"]}</Text></ImageBackground>}
+          {this.state["3"] && <ImageBackground source={require('../components/ico/breadcrumbs-btn-bg.png')} style={{ height: 80, width: 250,  alignItems: 'center', justifyContent: 'center',  }}><Text>{this.state["3"]}</Text></ImageBackground>}
+          {this.state["4"] && <ImageBackground  source={require('../components/ico/breadcrumbs-btn-bg.png')}style={{ height: 80, width: 250,  alignItems: 'center', justifyContent: 'center', }}><Text>{this.state["4"]}</Text></ImageBackground>}
         </View>
       </View>
 
